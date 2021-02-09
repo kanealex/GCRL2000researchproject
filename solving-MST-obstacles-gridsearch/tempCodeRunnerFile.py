68e4fa0fb6b15aa4e74b53f6ecbc72eac3acd386
Kanealex <72069221+Kanealex@users.noreply.github.com>
2021-02-09 20:17:57 +0800

    cleanup

--- solving-MST-obstacles-gridsearch/tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-for x in range(len(chromosomes)):
-    #     calc = hypot(chromosomes[x][0]-chromosomes[pointpos][0], chromosomes[x][1]-chromosomes[pointpos][1])
-    #     oldarray[x][pointpos] = calc
-    #     oldarray[pointpos][x] = calc
